fe6f488af73ac4884955e9723bbffa7979320b16
Serghei <lighsnow@gmail.com>
2021-05-31 21:13:57 +0300

    added new project MortalKombat

---
+++ main.js
@@ -0,0 +1,18 @@
+const firstName = 'Sonya';
+
+console.log(typeof firstName);
+
+const count = 4;
+
+console.log(typeof count);
+
+/*
+string
+number
+boolean
+undefined
+null
+object
+bigint
+symbol
+*/
